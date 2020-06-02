20351550410493269e581186995258162f1ef67b
aatbugis <aatbugis0111@gmail.com>
2020-06-02 17:43:00 +0800

    buat calss menu.java
    pas lgi ngerjain mati lampu, jadi terhenti dsni

---
+++ uasprojectandroid/app/src/main/java/com/example/uasprojectandroid/Menu.java
@@ -0,0 +1,9 @@
+package com.example.uasprojectandroid;
+
+public class Menu {
+    private String nama;
+    private String harga;
+    private String gambar;
+
+    public Menu(String datanama, String dataharga, String datagambar)
+}
