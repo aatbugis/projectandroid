8bc372eb7a350e09903a7b31aaf11413b13b82eb
aatbugis <aatbugis0111@gmail.com>
2020-06-05 13:55:04 +0800

    part4 selese, next detail activity
    part4 selese, next detail activity

--- uasprojectandroid/app/src/main/java/com/example/uasprojectandroid/MainActivity.java
+++ uasprojectandroid/app/src/main/java/com/example/uasprojectandroid/MainActivity.java
@@ -6,8 +6,16 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -32,5 +40,33 @@ public class MainActivity extends AppCompatActivity {
         parseJSON();
     }
     private void parseJSON() {
+        String url="https://jsonfadjryy.000webhostapp.com/koneksi.php";
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        jumdata = response.length();
+                        try {
+                            for (int i = 0; i < jumdata; i++) {
+                                JSONObject data = response.getJSONObject(i);
+                                String gambarmenu = data.getString("gambar");
+                                String namamenu = data.getString("nama");
+                                String hargamenu = data.getString("harga");
+                                menus.add(new Menu(namamenu, hargamenu, gambarmenu));
+                            }
+                            menuAdapter = new MenuAdapter(MainActivity.this, menus);
+                            recyclerView.setAdapter(menuAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+
+    });
+        requestQueue.add(request);
     }
 }
